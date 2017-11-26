415fea8c954bc1a0d774ca33750a1f1ea0d56ede
Doug Cross <doug@dougcrossdesign.com>
2017-11-25 21:17:51 -0500

    Inital Commit - Upload Files

---
+++ script.js
@@ -0,0 +1,40 @@
+function handleFormSubmission(){
+  $('.form-example input').on('keyup', function(e){
+    $(this).addClass('edited');
+  });
+
+  $('.form-example [type="submit"]').on('click.formValidation', function(e){
+    shouldPrevent = false;
+    errorList = [];
+
+
+    $form = $(this).parents('form');
+    $inputs = $form.find('input[required], textarea[required], select[required]');
+
+    $inputs.each(function(index, input){
+      errorMessageSelector = 'label[for="' + $(input).attr('id') + '"] .error-message';
+      $form.find(errorMessageSelector).removeAttr('aria-live');
+      error = $form.find(errorMessageSelector);
+      error.css('display', 'none');
+
+      if(!input.validity.valid){
+        error.css('display', 'inline-block');
+        shouldPrevent = true;
+        errorList.push(error);
+
+      }
+      else {
+        error.css('display', 'none');
+      }
+    });
+
+    if(!$form[0].checkValidity()){
+      e.preventDefault();
+      errorList[0].attr('aria-live', 'assertive');
+    }
+  });
+}
+
+$(document).ready(function(){
+  handleFormSubmission();
+});
